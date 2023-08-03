eb9c7da56a962568d74daddd16650e479605a758
dhendrawirawan <119018562+dhendrawirawan@users.noreply.github.com>
2023-08-02 22:50:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let Counter = randint(1, 6)
+loops.everyInterval(200, function () {
+    if (Counter == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else if (Counter == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # . . # .
+            # . . . #
+            . # # # .
+            `)
+    } else if (Counter == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else if (Counter == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # . . #
+            # . . . #
+            . # # # .
+            `)
+    } else if (Counter == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Counter == 6) {
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            # # # # #
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+loops.everyInterval(100, function () {
+    Counter = randint(1, 6)
+})
